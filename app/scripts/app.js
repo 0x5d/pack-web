43b0879ac7db148dcbab22f909dd669f1aabcefc
David Castillo <castillobgr@gmail.com>
2015-07-14 23:15:29 -0500

    Yeoman basic structure

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name packWebApp
+ * @description
+ * # packWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('packWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngTouch'
+  ]);
